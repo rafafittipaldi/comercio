59fa3bd15ecb1d2652a5bfecc865196078cc7093
rafael.fittipaldi <rafael.fittipaldi@SJ216V21S2.lab.infnet.com.br>
2020-02-01 18:11:42 -0200

    - Ajustes - Criacao do SQL

---
+++ src/main/java/edu/infnet/comercio/negocio/modelo/Produto.java
@@ -0,0 +1,118 @@
+package edu.infnet.comercio.negocio.modelo;
+
+public class Produto {
+
+	private Integer id;
+	
+	private String nome;
+	
+	private String descricao;
+	
+	private Double preco;
+	
+	public Produto() {}
+
+	public Produto(Integer id, String nome, String descricao, Double preco) {
+		this.id        = id;
+		this.nome      = nome;
+		this.descricao = descricao;
+		this.preco     = preco;
+	}
+
+	public Produto(String nome, String descricao, Double preco) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result      = 1;
+		
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((preco == null) ? 0 : preco.hashCode());
+		
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (this == obj)
+			return true;
+		
+		if (obj == null)
+			return false;
+		
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Produto other = (Produto) obj;
+		
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		
+		if (preco == null) {
+			if (other.preco != null)
+				return false;
+		} else if (!preco.equals(other.preco))
+			return false;
+		
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", preco=" + preco + "]";
+	}
+}
---
+++ src/main/resources/Comercio.sql
@@ -0,0 +1,32 @@
+use comercio;
+
+drop table produto;
+drop table usuario;
+
+create table produto (
+	id int not null primary key auto_increment,
+    foto varchar(80),
+    nome varchar(80),
+    descricao varchar(255),
+    preco double(10,2)
+);
+
+create table usuario (
+	id int not null primary key auto_increment,
+    login varchar(80),
+    senha varchar(80)
+);
+
+insert into usuario (login, senha)
+	values(
+		'rafael',
+        '1234'
+);
+
+insert into produto (foto, nome, descricao, preco)
+	values(
+		'iphone.jpeg',
+        'Iphone',
+        'Design inovador, totalmente em vidro. A câmera que o mundo inteiro adora, ainda melhor. O chip mais poderoso e inteligente em qualquer smartphone. Recarga sem fio simples de verdade.',
+        3099.00
+);
